ee10cf74d7ad623dcb456d2107eba736c6f85b8a
Chang Gatere <chang.gatere1@gmail.com>
2021-10-27 13:35:05 +0300

    making changes to the what we do section

--- js/srcipts.js
+++ js/srcipts.js
@@ -1,9 +1,77 @@
 $(document).ready(function() {
-    $("img").click(function() {
-    $("h4").show();
-    $("p").show();
+    $("#design").click(function() {
+    $("#designh4").show();
+    $("#designp").show();
+    $("#desp").show();
+    $("#deshide").show();
     });
 });
+$(document).ready(function() {
+    $("#deshide").click(function() {
+    $("#designh4").hide();
+    $("#designp").hide();
+    $("#desp").hide();
+    $("#deshide").hide();
+    });
+});
+$(document).ready(function() {
+    $("#development").click(function() {
+    $("#developmenth4").show();
+    $("#developmentp").show();
+    $("#devp").show();
+    $("#dehide").show();
+    });
+});
+$(document).ready(function() {
+    $("#dehide").click(function() {
+    $("#developmenth4").hide();
+    $("#developmentp").hide();
+    $("#devp").hide();
+    $("#dehide").hide();
+    });
+});
+$(document).ready(function() {
+    $("#development").click(function() {
+    $("#developmenth4").show();
+    $("#developmentp").show();
+    $("#devp").show();
+    $("#dehide").show();
+    });
+});
+$(document).ready(function() {
+    $("#dehide").click(function() {
+    $("#developmenth4").hide();
+    $("#developmentp").hide();
+    $("#devp").hide();
+    $("#dehide").hide();
+    });
+});
+$(document).ready(function() {
+    $("#product").click(function() {
+    $("#producth4").show();
+    $("#productp").show();
+    $("#prop").show();
+    $("#prohide").show();
+    });
+});
+$(document).ready(function() {
+    $("#prohide").click(function() {
+    $("#producth4").hide();
+    $("#productp").hide();
+    $("#prop").hide();
+    $("#prohide").hide();
+    });
+});
+
+
+
+
+
+
+
+
+
+
 $(document).ready(function() {
     $(":button").click(function() {
     alert("Thank you, we have recieved your input")
